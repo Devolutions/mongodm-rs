94f55f5bbdb901ffa6a2424d831723632d4e906f
Benoît CORTIER <benoit.cortier@fried-world.eu>
2020-06-17 16:43:40 -0400

    Add update operators

--- src/operator.rs
+++ src/operator.rs
@@ -1,6 +1,10 @@
 //! Static operators for queries to prevent invalid queries due to typos
 //!
-//! See [mongo manual](https://docs.mongodb.com/manual/reference/operator/query/).
+//! See mongo manual for [query operators](https://docs.mongodb.com/manual/reference/operator/query/)
+//! and [update operators](https://docs.mongodb.com/manual/reference/operator/update/).
+//!
+//! If an operator is missing, you can easily add it yourself (also, PR are welcomed) or use the hardcoded
+//! string like you would in a mongo shell.
 //!
 //! ```
 //! use mongodm::mongo::bson::doc;
@@ -37,8 +41,11 @@
 //! ```
 
 macro_rules! declare_operator {
-    ($ty:ident, $mongo_operator:literal) => {
+    ($ty:ident => $mongo_operator:literal) => {
         #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+        #[doc="Operator `"]
+        #[doc=$mongo_operator]
+        #[doc="`"]
         pub struct $ty;
 
         impl From<$ty> for ::std::string::String {
@@ -47,65 +54,121 @@ macro_rules! declare_operator {
             }
         }
     };
+    ($category:literal [ $doc_url:literal ] : $ty:ident => $mongo_operator:literal) => {
+        #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+        #[doc="["]
+        #[doc=$category]
+        #[doc="]("]
+        #[doc=$doc_url]
+        #[doc=") "]
+        #[doc="operator `"]
+        #[doc=$mongo_operator]
+        #[doc="`"]
+        pub struct $ty;
+
+        impl From<$ty> for ::std::string::String {
+            fn from(_: $ty) -> ::std::string::String {
+                ::std::string::String::from($mongo_operator)
+            }
+        }
+    };
+    ($category:literal [ $doc_url:literal ] : $( $ty:ident => $mongo_operator:literal, )+ ) => {
+        $( declare_operator! { $category [ $doc_url ] : $ty => $mongo_operator } )+
+    };
 }
 
-// Comparison
+// == Query operators == //
 
-declare_operator! { Equal, "$eq" }
-declare_operator! { GreaterThan, "$gt" }
-declare_operator! { GreaterThanEqual, "$gte" }
-declare_operator! { In, "$in" }
-declare_operator! { LesserThan, "$lt" }
-declare_operator! { LesserThanEqual, "$lte" }
-declare_operator! { NotEqual, "$ne" }
-declare_operator! { NoneIn, "$nin" }
+declare_operator! { "Comparison" ["https://docs.mongodb.com/manual/reference/operator/query/#comparison"]:
+    Equal => "$eq",
+    GreaterThan => "$gt",
+    GreaterThanEqual => "$gte",
+    In => "$in",
+    LesserThan => "$lt",
+    LesserThanEqual => "$lte",
+    NotEqual => "$ne",
+    NoneIn => "$nin",
+}
 
-// Logical
+declare_operator! { "Logical" ["https://docs.mongodb.com/manual/reference/operator/query/#logical"]:
+    And => "$and",
+    Not => "$not",
+    Nor => "$nor",
+    Or => "$or",
+}
 
-declare_operator! { And, "$and" }
-declare_operator! { Not, "$not" }
-declare_operator! { Nor, "$nor" }
-declare_operator! { Or, "$or" }
+declare_operator! { "Element" ["https://docs.mongodb.com/manual/reference/operator/query/#element"]:
+    Exists => "$exists",
+    Type => "$type",
+}
 
-// Element
+declare_operator! { "Evaluation" ["https://docs.mongodb.com/manual/reference/operator/query/#evaluation"]:
+    Expr => "$expr",
+    JsonSchema => "$jsonSchema",
+    Mod => "$mod",
+    Regex => "$regex",
+    Text => "$text",
+    Where => "$where",
+}
 
-declare_operator! { Exists, "$exists" }
-declare_operator! { Type, "$type" }
+declare_operator! { "Geospatial" ["https://docs.mongodb.com/manual/reference/operator/query/#geospatial"]:
+    GeoIntersects => "$geoIntersects",
+    GeoWithin => "$geoWithin",
+    Near => "$near",
+    NearSphere => "$nearSphere",
+}
 
-// Evaluation
+declare_operator! { "Array (query)" ["https://docs.mongodb.com/manual/reference/operator/query/#array"]:
+    All => "$all",
+    ElemMatch => "$elemMatch",
+    Size => "$size",
+}
 
-declare_operator! { Expr, "$expr" }
-declare_operator! { JsonSchema, "$jsonSchema" }
-declare_operator! { Mod, "$mod" }
-declare_operator! { Regex, "$regex" }
-declare_operator! { Text, "$text" }
-declare_operator! { Where, "$where" }
+declare_operator! { "Bitwise (query)" ["https://docs.mongodb.com/manual/reference/operator/query/#bitwise"]:
+    BitsAllClear => "$bitsAllClear",
+    BitsAllSet => "$bitsAllSet",
+    BitsAnyClear => "$bitsAnyClear",
+    BitsAnySet => "$bitsAnySet",
+}
 
-// Geospatial
+declare_operator! { "Comments" ["https://docs.mongodb.com/manual/reference/operator/query/#comments"]: Comment => "$comment" }
 
-declare_operator! { GeoIntersects, "$geoIntersects" }
-declare_operator! { GeoWithin, "$geoWithin" }
-declare_operator! { Near, "$near" }
-declare_operator! { NearSphere, "$nearSphere" }
+declare_operator! { "Projection" ["https://docs.mongodb.com/manual/reference/operator/query/#projection-operators"]:
+    ProjectFirst => "$",
+    Meta => "$meta",
+    Slice => "$slice",
+}
 
-// Array
+// == Update operators ==
 
-declare_operator! { All, "$all" }
-declare_operator! { ElemMatch, "$elemMatch" }
-declare_operator! { Size, "$size" }
+declare_operator! { "Fields" ["https://docs.mongodb.com/manual/reference/operator/update/#fields"]:
+    CurrentDate => "$currentDate",
+    Inc => "$inc",
+    Min => "$min",
+    Max => "$max",
+    Mul => "$mul",
+    Rename => "$rename",
+    Set => "$set",
+    SetOnInsert => "$setOnInsert",
+    Unset => "$unset",
+}
 
-// Bitwise
+declare_operator! { "Array (update)" ["https://docs.mongodb.com/manual/reference/operator/update/#array"]:
+    UpdateFirstDocument => "$",
+    UpdateAllDocuments => "$[]",
+    AddToSet => "$addToSet",
+    Pop => "$pop",
+    Pull => "$pull",
+    Push => "$push",
+    PullAll => "$pullAll",
+}
 
-declare_operator! { BitsAllClear, "$bitsAllClear" }
-declare_operator! { BitsAllSet, "$bitsAllSet" }
-declare_operator! { BitsAnyClear, "$bitsAnyClear" }
-declare_operator! { BitsAnySet, "$bitsAnySet" }
+declare_operator! { "Modifiers" ["https://docs.mongodb.com/manual/reference/operator/update/#modifiers"]:
+    Each => "$each",
+    Position => "$position",
+    Sort => "$sort",
+}
 
-// Comments
-
-declare_operator! { Comment, "$comment" }
-
-// Projection operations
-
-declare_operator! { Meta, "$meta" }
-declare_operator! { Slice, "$slice" }
+declare_operator! { "Bitwise (update)" ["https://docs.mongodb.com/manual/reference/operator/update/#bitwise"]:
+    Bit => "$bit",
+}
