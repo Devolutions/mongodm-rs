6f18c885af547497427767c502eb9b84b4d4e566
Benoît CORTIER <benoit.cortier@fried-world.eu>
2021-04-14 18:43:03 -0400

    fields! accepts nested structs

--- src/macros.rs
+++ src/macros.rs
@@ -48,19 +48,55 @@
 /// assert_eq!(a, b);
 /// ```
 ///
+/// Nested structs
+/// ```
+/// use mongodm::mongo::bson::doc;
+/// use mongodm::field;
+///
+/// struct Foo {
+///     bar: Bar,
+/// }
+///
+/// struct Bar {
+///     lorem: String,
+///     baz: Baz,
+/// }
+///
+/// struct Baz {
+///     dolor: String,
+/// }
+///
+/// assert_eq!(
+///     doc! { field!((bar in Foo).(lorem in Bar)): "ipsum" },
+///     doc! { "bar.lorem": "ipsum" },
+/// );
+///
+/// assert_eq!(
+///     doc! { field!(@@(bar in Foo).(baz in Bar).(dolor in Baz)): "sit amet" },
+///     doc! { "$$bar.baz.dolor": "sit amet" },
+/// );
+/// ```
+///
 /// If the field doesn't exist, compilation will fail.
 ///
 /// ```compile_fail
 /// use mongodm::mongo::bson::doc;
 /// use mongodm::field;
-/// use mongodm::operator::*;
 ///
 /// struct MyModel {
-///     bar: i64,
+///     foo: i64,
+///     bar: Bar,
 /// }
 ///
-/// // Doesn't compile because `foo` isn't a member of `MyModel`
-/// doc! { field!(foo in MyModel): 0 };
+/// struct Bar {
+///     baz: i64,
+/// }
+///
+/// // Doesn't compile because `baz` isn't a member of `MyModel`
+/// doc! { field!(baz in MyModel): 0 };
+///
+/// // Doesn't compile because `foo` isn't a member of `Bar`
+/// doc! { field!((bar in MyModel).(foo in Bar)): 0 };
 /// ```
 #[macro_export]
 macro_rules! field {
@@ -69,7 +105,7 @@ macro_rules! field {
         const _: fn() = || {
             let $type { $field: _, .. };
         };
-        stringify!($field)
+        stringify!( $field )
     }};
     ( @ $field:ident in $type:path ) => {{
         #[allow(unknown_lints, unneeded_field_pattern)]
@@ -84,6 +120,19 @@ macro_rules! field {
             let $type { $field: _, .. };
         };
         concat!("$$", stringify!($field))
+    }};
+    ( ( $field:ident in $type:path ) )  => {{
+        $crate::field!( $field in $type )
+    }};
+    // FIXME: ideally we want a compile-time string instead of format!
+    ( ( $field:ident in $type:path ) . $( $rest:tt ).+ ) => {{
+        format!("{}.{}", $crate::field!( $field in $type ), $crate::field!( $( $rest ).+ ))
+    }};
+    ( @ ( $field:ident in $type:path ) . $( $rest:tt ).+ ) => {{
+        format!("${}.{}", $crate::field!( $field in $type ), $crate::field!( $( $rest ).+ ))
+    }};
+    ( @ @ ( $field:ident in $type:path ) . $( $rest:tt ).+ ) => {{
+        format!("$${}.{}", $crate::field!( $field in $type ), $crate::field!( $( $rest ).+ ))
     }};
 }
 
@@ -145,4 +194,17 @@ macro_rules! f {
     ( @ @ $field:ident in $type:path ) => {{
         $crate::field!( @ @ $field in $type)
     }};
+    ( ( $field:ident in $type:path ) )  => {{
+        $crate::field!( ( $field in $type ) )
+    }};
+    ( ( $field:ident in $type:path ) . $( $rest:tt ).+ ) => {{
+        $crate::field!( ( $field in $type ) . $( $rest ).+ )
+    }};
+    ( @ ( $field:ident in $type:path ) . $( $rest:tt ).+ ) => {{
+        $crate::field!( @ ( $field in $type ) . $( $rest ).+ )
+    }};
+    ( @ @ ( $field:ident in $type:path ) . $( $rest:tt ).+ ) => {{
+        $crate::field!( @ @ ( $field in $type ) . $( $rest ).+ )
+    }};
 }
+
